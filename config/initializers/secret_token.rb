df4e40a81a88ae72994ded3da8a851d04da4fc85
David Wagenleiter <D.Wagenleiter@outlook.com>
2013-04-05 19:39:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testdreckulischnauze::Application.config.secret_token = 'd24880da8bd8125f615b74993a4903cb0e89ac6f94677242f4bd8eaf1657afacf75bf2544bc2ee3a25579431497aef06310c7529a0e8d9295c2c69f0bf1466de'
